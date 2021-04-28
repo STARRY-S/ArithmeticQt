1fa3159f83145e962129e969661835808cec0432
starry-s <hx916584160@gmail.com>
2021-04-28 12:41:00 +0800

    MSVC Test

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,11 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+#ifdef _MSC_BUILD
+    a.setFont(QFont("Microsoft YaHei", 11));
+#else
+    a.setFont(QFont("Noto Sans SC", 11));
+#endif
     MainWindow w;
     w.show();
     return a.exec();
