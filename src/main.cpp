c9eee202f0be139979dc8675499f7c7befba27be
STARRY-S <hx916584160@gmail.com>
2021-04-28 17:15:34 +0800

    Change default font to Noto Sans CJK TC in Unix.

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,15 @@
 #include "mainwindow.h"
-
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+	QApplication a(argc, argv);
 #ifdef _MSC_BUILD
-    a.setFont(QFont("Microsoft YaHei", 11));
+	a.setFont(QFont("Microsoft YaHei", 11));
 #else
-    a.setFont(QFont("Noto Sans SC", 11));
+	a.setFont(QFont("Noto Sans CJK TC", 11));
 #endif
-    MainWindow w;
-    w.show();
-    return a.exec();
+	MainWindow w;
+	w.show();
+	return a.exec();
 }
