d496165805a6872109beb08781dafa68dff2ac1a
STARRY-S <hx916584160@gmail.com>
2021-04-28 18:01:21 +0800

    Add application icon

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@ int main(int argc, char *argv[])
 #else
 	a.setFont(QFont("Noto Sans CJK TC", 11));
 #endif
+	a.setWindowIcon(QIcon("images/favicon.ico"));
 	MainWindow w;
 	w.show();
 	return a.exec();
