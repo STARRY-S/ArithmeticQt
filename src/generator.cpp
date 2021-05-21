861022002b6d15ad4efc68152b302b42d98de41f
STARRY-S <hx916584160@gmail.com>
2021-05-21 17:45:57 +0800

    rename file.

---
+++ src/generator.cpp
@@ -0,0 +1,37 @@
+#include "generator.h"
+
+// 生成表达式
+void Generator::generate(std::string &exp)
+{
+    switch (set) {
+        case ADD:
+        {
+            ;
+            break;
+        }
+        case SUB:
+        {
+            break;
+        }
+        case MUL:
+        {
+            break;
+        }
+        case DIV:
+        {
+            break;
+        }
+        case PAR:
+        {
+            break;
+        }
+        case NPA:
+        {
+            break;
+        }
+        default:
+        {
+            break;
+        }
+    }
+}
