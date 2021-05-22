7a66ccd8f31a85f7363ffaad740340cde94c3fdd
STARRY-S <hx916584160@gmail.com>
2021-05-22 21:40:26 +0800

    Simplified Chinese.

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
 #ifdef _MSC_BUILD
         a.setFont(QFont("Microsoft YaHei", 11));
 #else
-        a.setFont(QFont("Noto Sans CJK TC", 11));
+        a.setFont(QFont("Noto Sans CJK SC", 11));
 #endif
         a.setWindowIcon(QIcon("images/favicon.ico"));
         MainWindow w;
