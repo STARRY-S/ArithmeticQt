36e8d27e87769bb68f7e54c034f04d8f4eb0a583
STARRY-S <hx916584160@gmail.com>
2021-05-22 17:23:34 +0800

    rewrite arithmetic. (change indentation)

--- src/arithmetic.hpp
+++ src/arithmetic.hpp
@@ -1,44 +1,35 @@
 #ifndef ARITHMETIC_H
 #define ARITHMETIC_H
 
+#include "generator.hpp"
+#include "calculator.hpp"
+
 #include <iostream>
 #include <string>
 #include <vector>
-#include <QRandomGenerator>
 #include <QString>
 
-class Arithmetic {
-public:
-        Arithmetic();
-        ~Arithmetic();
-
-        QString getQuestion(int i);
-        QString getAnswer(int i);
-        void generate();
-        unsigned char getSet();
-        void setSet(unsigned char);
-        int getQuestionNum();
-        void setQuestionNum(int);
-        void setDifficulty(int max, int min, bool n, bool p);
-        int openFile(std::string name);
-
+class Arithmetic : public Generator, public Calculator {
 private:
-        int questionNum;                // 题目数量
-        int maxNum;                     // 随机数最大值
-        int minNum;                     // 最小值
-        bool hasNegative;               // 结果是否有正负
-        bool hasPoint;                  // 结果是否有小数
-        unsigned char set;              // 当前设置（加减乘除、混合）
-        std::vector<QString> questionList;
-        std::vector<QString> answerList;
+    int questionNum;                // 题目数量
+    std::vector<QString> questionList;
+    std::vector<QString> answerList;
 
-        QString generateBySet(int answer_pos);
-        QString generateMulti(int answer_pos);
-        int getRandNum(unsigned int a);
-        int getRandNum(unsigned int a, unsigned int b);
-        void checkLabel();
-        QString calculateAnswer(int i);
-        bool checkLineValid(std::string str);
+    bool checkLineValid(std::string str);
+
+public:
+    Arithmetic();
+    ~Arithmetic();
+
+    QString getQuestion(int i);
+    QString getAnswer(int i);
+    void generate();
+    unsigned char getSet();
+    void setSet(unsigned char);
+    int getQuestionNum();
+    void setQuestionNum(int);
+    void setDifficulty(int max, int min, bool n, bool p);
+    int openFile(std::string name);
 };
 
 #endif
